11931e9cd290b99c41e00659bae4d2f43d300b69
Adam Jankowski <ghost17814@gmail.com>
2023-05-15 14:54:34 -0400

    weather

--- APIsAndJSON/OpenWeatherMapAPI.cs
+++ APIsAndJSON/OpenWeatherMapAPI.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,14 @@ namespace APIsAndJSON
 {
     internal class OpenWeatherMapAPI
     {
+        public static void GetWeather()
+        {
+            var client = new HttpClient();           
+            var cityName = "Benton";
+            var weatherUrl = $"https://api.openweathermap.org/data/2.5/weather?q={cityName}&appid={ApiKey.Key}&units=imperial";
+            var weatherRepsonse = client.GetStringAsync(weatherUrl).Result;
+            var weatherObj = JObject.Parse(weatherRepsonse);
+            Console.WriteLine(weatherObj["main"]["temp"]);
+        }
     }
 }
--- APIsAndJSON/Program.cs
+++ APIsAndJSON/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace APIsAndJSON
+﻿using Newtonsoft.Json.Linq;
+using System.Net.Http;
+using static System.Net.WebRequestMethods;
+
+namespace APIsAndJSON
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Console.WriteLine("The great Ron West once said"+"\n");
+            //QuoteGenerator.YeQuote();
+            //QuoteGenerator.RonWords();
+            OpenWeatherMapAPI.GetWeather();
         }
     }
 }
